a4a8c43eda191ea1a57abddf0ace7b4e79c0487d
Vijay-CPP <vijaytamada02@gmail.com>
2023-11-13 07:54:14 +0530

    Dynamic Home page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
+import { useUserAuth } from "../context/UserAuthContext";
 
 const Homepage = () => {
+  const { user } = useUserAuth();
+
   return (
     <div>
       <Navbar />
@@ -15,19 +18,36 @@ const Homepage = () => {
             Keep track of your daily habits and maintain your streaks with
             Streak Keeper.
           </p>
-          <div className="flex flex-col justify-center md:flex-row items-center md:space-x-4">
-            <Link to={"/login"}>
-              <h1 className="text-base md:text-xl bg-purple-400 text-white font-medium px-4 md:px-6 py-2 rounded-md hover:bg-purple-500">
-                Login
-              </h1>
-            </Link>
 
-            <Link to={"/signup"}>
-              <h1 className="text-base md:text-xl border-[1px] px-4 md:px-6 py-2 bg-purple-200 rounded-md hover:bg-purple-300 text-gray-800 mt-2 md:mt-0">
-                Sign Up
-              </h1>
-            </Link>
-          </div>
+          {!user ? (
+            <div className="flex flex-col justify-center md:flex-row items-center md:space-x-4">
+              <Link to={"/login"}>
+                <h1 className="text-base md:text-xl bg-purple-400 text-white font-medium px-4 md:px-6 py-2 rounded-md hover:bg-purple-500">
+                  Login
+                </h1>
+              </Link>
+
+              <Link to={"/signup"}>
+                <h1 className="text-base md:text-xl border-[1px] px-4 md:px-6 py-2 bg-purple-200 rounded-md hover:bg-purple-300 text-gray-800 mt-2 md:mt-0">
+                  Sign Up
+                </h1>
+              </Link>
+            </div>
+          ) : (
+            <div className="flex flex-col justify-center md:flex-row items-center md:space-x-4">
+              <Link to={"/dashboard"}>
+                <h1 className="text-base md:text-xl bg-purple-400 text-white font-medium px-4 md:px-6 py-2 rounded-md hover:bg-purple-500">
+                  Go to Dashboard
+                </h1>
+              </Link>
+
+              <Link to={"/logout"}>
+                <h1 className="text-base md:text-xl border-[1px] px-4 md:px-6 py-2 bg-purple-200 rounded-md hover:bg-purple-300 text-gray-800 mt-2 md:mt-0">
+                  Logout
+                </h1>
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </div>
